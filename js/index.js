1249dbd93d07776e8324d81d2a744363ecc217b3
ralph-dev <ralphpal@hotmail.com>
2016-06-05 03:07:35 -0400

    Modified conversation switch

--- js/index.js
+++ js/index.js
@@ -14,17 +14,6 @@
   };
 
 
-
-  //This code covers the on click event
-  var ul = document.getElementsByClassName("list-friends");  // Parent
-
-  ul.addEventListener('click', function(e) {
-    console.log("Hello");
-    if (e.target.tagName === 'LI'){
-        alert(e.target.id);  // Check if the element is a LI
-    }
-  });
-
   // Response
   NYLM = ["Couldn't find anything :P, Try stackoverflow","The pip function might help", "Have you tried the cat function?","Well, I did create Linux didn't I :P"]; // Response
 
